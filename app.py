fc2a2bdd0b84dd2268f661df4350d688f8bef270
Muazzeem <muazzem.mamun @gmail.com>
2023-03-04 09:29:29 +0600

    config

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from sqlalchemy import create_engine, URL, MetaData
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Docker & Flask</h2>'
+    db_url = URL.create(
+        "postgresql+psycopg2",
+        username="postgres",
+        password="Fr@ctalslab3110",
+        host="10.27.112.14",
+        database="postgres",
+    )
+    engine = create_engine(db_url, connect_args={'connect_timeout': 10})
+    metadata = MetaData()
+    metadata.reflect(engine)
+    return f'<h1>{metadata}</h2>'
 
 
 if __name__ == "__main__":
