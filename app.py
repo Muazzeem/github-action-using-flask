f4ffd0da904bd3f7d9cef336028525fc0a8b3f14
Muazzeem <muazzem.mamun @gmail.com>
2023-03-04 09:33:24 +0600

    config

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    db_url = URL.create(
-        "postgresql+psycopg2",
-        username="postgres",
-        password="Fr@ctalslab3110",
-        host="10.27.112.14",
-        database="postgres",
-    )
-    engine = create_engine(db_url, connect_args={'connect_timeout': 10})
-    metadata = MetaData()
-    metadata.reflect(engine)
-    return f'<h1>{metadata}</h2>'
+    # db_url = URL.create(
+    #     "postgresql+psycopg2",
+    #     username="postgres",
+    #     password="Fr@ctalslab3110",
+    #     host="10.27.112.14",
+    #     database="postgres",
+    # )
+    # engine = create_engine(db_url, connect_args={'connect_timeout': 10})
+    # metadata = MetaData()
+    # metadata.reflect(engine)
+    return '<h1>AAAAAAA</h2>'
 
 
 if __name__ == "__main__":
