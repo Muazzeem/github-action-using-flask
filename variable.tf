0f99cad5d9798936ada108c7adf0450eca99163b
Muazzeem <muazzem.mamun @gmail.com>
2023-03-11 15:36:04 +0600

    addd code

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "project" {
+  default = "saifuls-playground"
+}
+variable "stage" {
+  default = "test"
+}
+variable "network" {
+  default = "default"
+}
+variable "db_pass" {
+  type    = string
+  default = "Fr@ctalslab3110"
+}
