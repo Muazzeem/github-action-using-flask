d3dc6c6a731598e966cb23de8ad01fe4f8366dbe
Muazzeem <muazzem.mamun @gmail.com>
2023-04-04 15:01:53 +0600

    aaa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 // const github = require('@actions/github');
 
 try {
-    const stage = core.getInput('to-greet');
+    const stage = core.getInput('who-to-greet');
     console.log(`Hello ${stage}!`);
     core.setOutput("stage", stage);
 } catch (error) {
