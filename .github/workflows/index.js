7ee4f00b642f84bff33d3d41ee1b7162ea6c072a
Muazzeem <muazzem.mamun @gmail.com>
2023-04-04 12:19:08 +0600

    add js file

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,10 +1,15 @@
-const { execSync } = require('child_process');
-const command = process.env.INPUT_TEST_COMMAND;
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
-  execSync(command, { stdio: 'inherit' });
-  console.log("command:", command)
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
-  console.error(error);
-  process.exit(1);
+  core.setFailed(error.message);
 }
