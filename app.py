b419ef586a0d94e457feff56c4ace5b315af1eec
Muazzeem <muazzem.mamun @gmail.com>
2023-04-05 16:01:33 +0600

    aaa

--- app.py
+++ app.py
@@ -6,16 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    # db_url = URL.create(
-    #     "postgresql+psycopg2",
-    #     username="postgres",
-    #     password="Fr@ctalslab3110",
-    #     host="10.27.112.14",
-    #     database="postgres",
-    # )
-    # engine = create_engine(db_url, connect_args={'connect_timeout': 10})
-    # metadata = MetaData()
-    # metadata.reflect(engine)
+    return '<h1>Hello World</h2>'
+
+@app.route('/error')
+def error_geek():
+    a
     return '<h1>Hello World</h2>'
 
 
--- main.tf
+++ main.tf
@@ -3,37 +3,37 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
-
-resource "google_vpc_access_connector" "default" {
-  name          = "serverless-vpc-connector"
-  network       = "projects/${var.project}/global/networks/default"
-  ip_cidr_range = "10.8.0.0/28"
-}
-
-resource "random_id" "db_name_suffix" {
-  byte_length = 4
-}
-
-resource "google_sql_database_instance" "instance" {
-  provider = google-beta
-  project  = var.project
-
-  name             = "private-instance-${random_id.db_name_suffix.hex}"
-  region           = "us-central1"
-  database_version = "POSTGRES_14"
-  root_password    = var.db_pass
-
-  settings {
-    tier = "db-f1-micro"
-    ip_configuration {
-      ipv4_enabled    = false
-      private_network = "projects/${var.project}/global/networks/default"
-
-      enable_private_path_for_google_cloud_services = true
-    }
-  }
-}
-output "db_info" {
-  value     = google_sql_database_instance.instance.name
-}
+#
+#resource "google_vpc_access_connector" "default" {
+#  name          = "serverless-vpc-connector"
+#  network       = "projects/${var.project}/global/networks/default"
+#  ip_cidr_range = "10.8.0.0/28"
+#}
+#
+#resource "random_id" "db_name_suffix" {
+#  byte_length = 4
+#}
+#
+#resource "google_sql_database_instance" "instance" {
+#  provider = google-beta
+#  project  = var.project
+#
+#  name             = "private-instance-${random_id.db_name_suffix.hex}"
+#  region           = "us-central1"
+#  database_version = "POSTGRES_14"
+#  root_password    = var.db_pass
+#
+#  settings {
+#    tier = "db-f1-micro"
+#    ip_configuration {
+#      ipv4_enabled    = false
+#      private_network = "projects/${var.project}/global/networks/default"
+#
+#      enable_private_path_for_google_cloud_services = true
+#    }
+#  }
+#}
+#output "db_info" {
+#  value = google_sql_database_instance.instance.name
+#}
 
