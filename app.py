ae8dafdb579b2050f2f29241759d3627f0789576
Muazzeem <muazzem.mamun @gmail.com>
2023-04-05 16:31:36 +0600

    sleep function

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask
 from sqlalchemy import create_engine, URL, MetaData
 
@@ -13,6 +14,11 @@ def error_geek():
     a
     return '<h1>Hello World</h2>'
 
+@app.route('/sleep')
+def sleep_two_minutes():
+    print("Sleeping for 2 minutes...")
+    time.sleep(30)  # sleep for 120 seconds (2 minutes)
+    return '<h1>Done sleeping!</h2>'
 
 if __name__ == "__main__":
     app.run(debug=True)
