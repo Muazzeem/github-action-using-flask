b1992de3460bc97380511712eabc2afe361893f8
Muazzeem <muazzem.mamun @gmail.com>
2023-04-05 16:01:33 +0600

    aaa

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from sqlalchemy import create_engine, URL, MetaData
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_geek():
+    return '<h1>Hello World</h2>'
+
+@app.route('/error')
+def error_geek():
+    a
     return '<h1>Hello World</h2>'
 
 
